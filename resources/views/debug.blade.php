ad127f213b50a125928a9c536a6188af874b8507
Erik Rundberg <erik@rundberg.ax>
2021-05-05 15:54:32 +0200

    Debug page fixed

--- resources/views/debug.blade.php
+++ resources/views/debug.blade.php
@@ -3,9 +3,6 @@
     @php
     echo "<h1>Debug details</h1>";
 
-    var_dump(getBaseUrl());
-    var_dump(getCurrentUrl());
-    var_dump(getRoutePath());
     var_dump($_SERVER);
     @endphp
 @stop
